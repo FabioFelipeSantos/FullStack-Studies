2c2b96247eba6fa2582d2800f8190b3e493bcb00
Fabio <fabiotrabmat@gmail.com>
2024-03-30 19:43:42 -0300

    autoCommit 03/30/2024 (19:43), M Front-end-CodeCademy/Examples/OtherThings/borderValues/index.html Front-end-CodeCademy/Examples/OtherThings/borderValues/styles.css Front-end-CodeCademy/Examples/OtherThings/overflowValues/script.js

--- Front-end-CodeCademy/Examples/OtherThings/overflowValues/script.js
+++
@@ -1,6 +0,0 @@
-const div = document.querySelector('.container');
-const bdWidth = window.getComputedStyle(div).getPropertyValue('border-radius');
-
-console.log(bdWidth)
-
-const p1 = document.createElement('p');
